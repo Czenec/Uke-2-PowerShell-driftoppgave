b97b1794bd704cd34a24585cce837756350a0a9e
Czenec <104144774+Czenec@users.noreply.github.com>
2023-01-24 13:16:33 +0100

    New DHCP scope
    How to create the DHCP scope specified in the task

---
+++ New DHCP scope.ps1
@@ -0,0 +1,7 @@
+$ScopeName = "Scope01"
+$HostMin = "172.16.40.1"
+$HostsMax = "172.16.40.62"
+$SubnetMask = "255.255.255.192"
+$LeaseDuration = "0.00:30:00"
+
+Add-DhcpServerv4Scope -Name $ScopeName -StartRange $HostMin -EndRange $HostsMax -SubnetMask $SubnetMask -LeaseDuration $LeaseDuration
